c6028ae9fc34a23c57834e94da56c795be46e032
Ryusei Yadoumaru <ryusei0942@outlook.jp>
2022-07-21 21:18:01 +0900

    クラス名取得したいよ

--- Cplus_20_Study/Noexcept.cpp
+++ Cplus_20_Study/Noexcept.cpp
@@ -1,11 +1,11 @@
-// noexcept
-// C++11で導入された。
-//throwキーワードの例外仕様の代替
-//関数がどの例外を送出する可能性があるか列挙するのではなく
-//例外の可能性のあるかないかを指定する
-//例外を送出する可能性がある関数：noexcept(false)
-//例外を送出する可能性がない関数：noexcept(true) or noexcept
-// https://cpprefjp.github.io/lang/cpp11/noexcept.html
+//　noexcept
+//　C++11で導入された。
+//　throwキーワードの例外仕様の代替
+//　関数がどの例外を送出する可能性があるか列挙するのではなく
+//　例外の可能性のあるかないかを指定する
+//　例外を送出する可能性がある関数：noexcept(false)
+//　例外を送出する可能性がない関数：noexcept(true) or noexcept
+//　https://cpprefjp.github.io/lang/cpp11/noexcept.html
 
 
 class Player
---
+++ Cplus_20_Study/Test_GetClassName.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <typeinfo>
+#include <string>
+using namespace std;
+
+class Foo
+{
+	int a = 0;
+	int b = 0;
+};
+
+struct  test
+{
+	int a = 0;
+	int b = 0;
+};
+
+enum class Enum
+{
+	one,
+	two
+};
+
+int main()
+{
+	Foo obj_1;
+	string name_class = typeid(obj_1).name();
+	cout << name_class << endl;
+
+	test obj_2;
+	string name_struct = typeid(obj_2).name();
+	cout << name_struct << endl;
+
+	Enum obj_3;
+	string name_enumclass = typeid(obj_3).name();
+	cout << name_enumclass << endl;
+
+	return 0;
+}
